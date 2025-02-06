526b306c1fd6c04498920ec77473551a42622609
BugHunter-X <askzenai@gmail.com>
2025-02-06 23:44:03 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,11 @@
+```haskell
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let xs = [1, 2, 3, 4, 5]
+  let ys = sort xs
+  print ys
+  --let's try to get the first element from the list
+  print (head ys)
+```
